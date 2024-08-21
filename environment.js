c6efb1d05bfd4dbcf98326408eead288ca6d540d
Dermot Canniffe <dermot.canniffe@gmail.com>
2024-08-21 18:09:10 +0100

    Some modifications

--- environment.js
+++ environment.js
@@ -1,8 +1,20 @@
+import { exec } from 'child_process';
+import { config } from 'dotenv/config';
 
+exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+    if (err) {
+        // handle your error
+    }
 
-var a = 'route 3';
-if(a === 'route 1' || 'route 2'){
-  console.log('true')
-}else{
-  console.log('false')
-}
+    if (typeof stdout === 'string' && (stdout.trim() === 'main')) {
+        console.log(`The branch is main`);
+        // Call your function here conditionally as per branch
+        cross-env CUCUMBER_PUBLISH_TOKEN = 183135b2-77a0-4d98-9070-8a1c96a6e4e2
+    }
+    if (typeof stdout === 'string' && (stdout.trim() === 'main')) {
+        console.log(`The branch is main`);
+        // Call your function here conditionally as per branch
+        config.
+        process.env.CUCUMBER_PUBLISH_TOKEN = 183135b2-77a0-4d98-9070-8a1c96a6e4e2
+    }
+});
